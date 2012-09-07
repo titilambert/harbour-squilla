a3647c7331c18b4af48a62b8eefd33e267477db5
Thibault Cohen <titilambert@gmail.com>
2012-09-06 23:25:16 -0400

    * Prepare version 1.6.5

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ for fpath in glob.glob('*/*.py[c|o]'):
 for fpath in glob.glob('*/*/*.py[c|o]'):
     os.remove(fpath)
  
-changes = '* Fix call notifier'
+changes = '* Fix my poor english'
  
 setup(name='heysms',
-      version='1.6.4',
+      version='1.6.5',
       license='GNU GPLv2',
       description="HeySms forwards sms to your Bonjour account.",
       long_description="HeySms forwards sms to your Bonjour account. You can also answer by Bonjour.",
