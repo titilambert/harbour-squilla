9e4bc8ddf123fa7aac4c01e501e0a2b887dee4f1
Stefan Siegl <stesie@brokenpipe.de>
2012-11-16 23:47:26 +0100

    Add German translations.

---
+++ i18n/heysms_de.ts
@@ -0,0 +1,250 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>Call_listener</name>
+    <message>
+        <location filename="heysms/lib/call_listener.py" line="50"/>
+        <source>New incoming call from: %s</source>
+        <translation>Neuer eingehender Anruf von: %s</translation>
+    </message>
+</context>
+<context>
+    <name>Central_widget</name>
+    <message>
+        <location filename="heysms/heysms.py" line="99"/>
+        <source>Select your Bonjour contact id :</source>
+        <translation>Wählen Sie Ihre Bonjour Kontakt-ID: </translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="101"/>
+        <source>Friend list :</source>
+        <translation>Freundesliste:</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="111"/>
+        <source>Looking for Bonjour contacts ...</source>
+        <translation>Suche Bonjour Kontake ...</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="115"/>
+        <source>No Bonjour contacts found !</source>
+        <translation>Keine Bonjour Kontakte gefunden!</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="133"/>
+        <source>Bonjour contacts loaded !</source>
+        <translation>Bonjour Kontakte geladen!</translation>
+    </message>
+</context>
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="heysms/lib/config.py" line="192"/>
+        <source>Please don&apos;t set USB mode</source>
+        <translation>Bitte USB Modus nicht setzen</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/config.py" line="203"/>
+        <source>USB network is currently used ...</source>
+        <translation>USB Netzwerk wird gerade verwendet ...</translation>
+    </message>
+</context>
+<context>
+    <name>Config_dialog</name>
+    <message>
+        <location filename="heysms/lib/config.py" line="396"/>
+        <source>Switch in Silent mode when HeySms starts</source>
+        <translation>Stumm-Modus wählen, wenn HeySms startet</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/config.py" line="397"/>
+        <source>Use Smssend to send Sms</source>
+        <translation>Smssend verwenden um SMS zu senden</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/config.py" line="398"/>
+        <source>Activate USB networking</source>
+        <translation>USB Netzwork aktivieren</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/config.py" line="399"/>
+        <source>Active Controller contact</source>
+        <translation>Aktiver Controller Kontakt</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/config.py" line="400"/>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/config.py" line="401"/>
+        <source>Preferences</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/config.py" line="429"/>
+        <source>You need to install smssend</source>
+        <translation>Sie müssen smssend installieren</translation>
+    </message>
+</context>
+<context>
+    <name>Controller</name>
+    <message>
+        <location filename="heysms/lib/controller.py" line="200"/>
+        <source>Command `%s&apos; not found. Type `help&apos; to see available commands</source>
+        <translation>Befehl `%s&apos; nicht gefunden. Für Liste der verfügbaren Befehle `help&apos; eingeben </translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="202"/>
+        <source>Error in function `function_%s&apos;: %s</source>
+        <translation>Fehler in Funktion `function_%s&apos;: %s</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="209"/>
+        <source>
+HeySms Help
+===========
+
+Show this help	: help
+Add contact	: add $CONTACT_ID$. Use `search&apos; command before
+Del contact	: del $CONTACT_ID$. Use `show&apos; command before
+Search contact	: search $CONTACT_NAME$
+Show contacts	: show
+Echo your msg	: echo $MESSAGE$
+</source>
+        <translation>
+HeySms Hilfe
+===========
+
+Diese Hilfe zeigen:	help
+Kontakt hinzufügen:	add $KONTAKT_ID$. Vorher `search&apos; Befehl verwenden
+Kontakt entfernen:	del $KONTAKT_ID$. Vorher `show&apos; Befehl verwenden
+Kontakt suchen:	search $KONTAKT_NAME$
+Kontakte anzeigen:	show
+Nachricht zurücksenden:	echo $NACHRICHT</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="232"/>
+        <source>Please use `search&apos; command before</source>
+        <translation>Bitte vorher `search&apos; Befehl verwenden</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="266"/>
+        <source>Bad ID: %s</source>
+        <translation>Ungültige ID: %s</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="269"/>
+        <source>ID not found: %s</source>
+        <translation>ID nicht gefunden: %s</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="246"/>
+        <source>
+Contact `%s&apos; already activated
+</source>
+        <translation>Kontakt `%s&apos; bereits aktiv</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="254"/>
+        <source>
+Contact `%s&apos; activated
+</source>
+        <translation>Kontakt `%s&apos; aktiviert</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="261"/>
+        <source>Please use `show&apos; command before</source>
+        <translation>Bitte vorher `show&apos; Befehl verwenden</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="275"/>
+        <source>
+Contact `%s&apos; deleted
+</source>
+        <translation>Kontakt `%s&apos; gelöscht</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="287"/>
+        <source>
+		Search result
+  IDs	    Names				Numbers
+</source>
+        <translation>
+		Suchergebnis
+  IDs	    Namen				Nummern
+</translation>
+    </message>
+    <message>
+        <location filename="heysms/lib/controller.py" line="294"/>
+        <source>
+		Active contacts
+ IDs	  Favorite		Names				Numbers
+</source>
+        <translation>
+		Aktive Kontakte
+ IDs	  Favorit		Namen				Nummern
+</translation>
+    </message>
+</context>
+<context>
+    <name>MenuBar</name>
+    <message>
+        <location filename="heysms/heysms.py" line="62"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="63"/>
+        <source>Add &amp;friend</source>
+        <translation>&amp;Freund hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="64"/>
+        <source>&amp;About</source>
+        <translation>&amp;Info</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="65"/>
+        <source>&amp;Quit</source>
+        <translation>B&amp;eenden</translation>
+    </message>
+</context>
+<context>
+    <name>Scheduler</name>
+    <message>
+        <location filename="heysms/lib/scheduler.py" line="126"/>
+        <source>Avahi error, please restart HeySms</source>
+        <translation>Avahi Fehler, bitte HeySms neu starten</translation>
+    </message>
+</context>
+<context>
+    <name>Ui_MainWindow</name>
+    <message>
+        <location filename="heysms/heysms.py" line="216"/>
+        <source>Done</source>
+        <translation>Fertig</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="252"/>
+        <source>Add a friend</source>
+        <translation>Freund hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="253"/>
+        <source>Select a friend</source>
+        <translation>Freund auswählen</translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="285"/>
+        <source>&lt;center&gt;&lt;h2&gt;HeySms&lt;/h2&gt;&lt;/center&gt;&lt;center&gt;Version: %(version)s&lt;/center&gt;&lt;br/&gt;Visite web site: &lt;a href=&quot;http://talk.maemo.org/showthread.php?t=84705&quot;&gt;Here&lt;/a&gt; &lt;br/&gt;Report bugs: &lt;a href=&quot;https://github.com/titilambert/HeySms/issues?milestone=&amp;page=1&amp;state=open&quot;&gt;Here&lt;/a&gt; &lt;br/&gt;Thanks: &lt;ul&gt;&lt;li&gt;My Doudoune&lt;/li&gt;&lt;li&gt;David Rodigari&lt;/li&gt;&lt;li&gt;Oskar Welzl&lt;/li&gt;&lt;li&gt;Stefan Siegl&lt;/li&gt;&lt;/ul&gt; </source>
+        <translation>&lt;center&gt;&lt;h2&gt;HeySms&lt;/h2&gt;&lt;/center&gt;&lt;center&gt;Version: %(version)s&lt;/center&gt;&lt;br/&gt;Website besuchen: &lt;a href=&quot;http://talk.maemo.org/showthread.php?t=84705&quot;&gt;hier&lt;/a&gt; &lt;br/&gt;Programmfehler melden: &lt;a href=&quot;https://github.com/titilambert/HeySms/issues?milestone=&amp;page=1&amp;state=open&quot;&gt;hier&lt;/a&gt; &lt;br/&gt;Danke: &lt;ul&gt;&lt;li&gt;My Doudoune&lt;/li&gt;&lt;li&gt;David Rodigari&lt;/li&gt;&lt;li&gt;Oskar Welzl&lt;/li&gt;&lt;li&gt;Stefan Siegl&lt;/li&gt;&lt;/ul&gt; </translation>
+    </message>
+    <message>
+        <location filename="heysms/heysms.py" line="297"/>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+</context>
+</TS>
